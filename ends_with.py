c84645a490cbacf26f6252781ebe35764e3b771d
NareRamazyan <128586114+NareRamazyan@users.noreply.github.com>
2023-11-29 13:43:47 +0400

    Update ends_with.py

--- ends_with.py
+++ ends_with.py
@@ -1,3 +1,10 @@
+"""
+ex.8
+write function for the following string operation
+endswith(str_obj, sub_string)
+this function should do the same as a builtin string function do
+"""
+
 def ends_with(str_obj, sub_string):
     if len(sub_string) > len(str_obj):
         return False
