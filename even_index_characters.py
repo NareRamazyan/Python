814600ab1c2310e2f3466ac86ee38412ff7404cf
NareRamazyan <128586114+NareRamazyan@users.noreply.github.com>
2023-11-29 13:45:02 +0400

    Update even_index_characters.py

--- even_index_characters.py
+++ even_index_characters.py
@@ -1,3 +1,10 @@
+"""
+ex.3
+Print characters from a string that are present at an even index number
+Orginal String is  test_text
+Printing only even index chars
+"""
+
 string = "test_text"
 iven_index_chars = string[::2]
 print("original string:", string)
