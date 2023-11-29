487f29268094ceae745a8d5aa9f4acf94ba57c05
NareRamazyan <128586114+NareRamazyan@users.noreply.github.com>
2023-11-29 13:48:24 +0400

    Update palindrom_number.py

--- palindrom_number.py
+++ palindrom_number.py
@@ -1,8 +1,16 @@
+"""
+ex.5
+Check Palindrome Number
+Write a program to check if the given number is a palindrome number.
+A palindrome number is a number that is the same after reverse.
+For example, 545, is the palindrome numbers
+"""
+
 number = int(input("number = "))
 
 num_str = str(number)
 
 if num_str == num_str[::-1]:
-    print("the number is palondrom")
+    print("the number is polondrome")
 else:
-    print("the number is not palindrom")
+    print("the number is not polindrome")
